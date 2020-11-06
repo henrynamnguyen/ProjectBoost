a8f6e9e33ac46bd6ff93450150be998abc98c808
renaisse <ngnhatnam188@gmail.com>
2020-11-06 05:21:35 -0500

    Added levels, sounds

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -2,13 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
     AudioSource audioSource;
-    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float rcsThrust = 250f;
     [SerializeField] float mainThrust = 10f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
+
+    enum State { Alive, Dying, Transcending };
+    [SerializeField]State state = State.Alive;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,19 +28,61 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
-    private void Thrust()
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive) { return; } //ignore collision when dead
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstScene", 1.0f);
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        Invoke("LoadNextScene", 1.0f);
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            if (!audioSource.isPlaying) // so the audio frames do not layer on top of one another to create weird sound.
-            {
-                audioSource.Play();
-            }
+            ApplyThrust();
 
         }
         else if (!Input.GetKey(KeyCode.Space))
@@ -40,7 +91,16 @@ public class Rocket : MonoBehaviour
         }
     }
 
-    private void Rotate()
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying) // so the audio frames do not layer on top of one another to create weird sound.
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    private void RespondToRotateInput()
     {
         rigidBody.freezeRotation = true; //after this line, we assume manual control of the rotarion
         float rotationThisFrame = rcsThrust * Time.deltaTime;
